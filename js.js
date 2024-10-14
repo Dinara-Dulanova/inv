f2f5b5687e8d5f05c7a77fb570412695fdab1588
dulanova <dinara-dulanova@yandex.ru>
2024-10-14 17:20:21 +0300

    add: contacts

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
 function showRestaurantMap() {
     window.open('https://yandex.ru/maps/org/syrovarnya/98123860242/?ll=30.356086%2C59.937215&z=17.05');
 }
+
+function chatWithOrganizator() {
+    window.open('https://wa.me/79049876547');
+}
